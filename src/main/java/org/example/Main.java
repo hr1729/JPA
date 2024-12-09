60432a44f6239a9bccc3829c68b5325cfda12275
hr1729 <65075246+hr1729@users.noreply.github.com>
2024-12-09 22:36:18 +0530

    learning about the anntation in the hibernate

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,22 @@
 package org.example;
 
 
+import org.example.domain.Student;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf= Persistence.createEntityManagerFactory("org.example");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Student hri = new Student("hri","a",new Date());
+        em.persist(hri);
+
+        em.getTransaction().commit();
+
     }
 }
--- src/main/java/org/example/domain/Student.java
+++ src/main/java/org/example/domain/Student.java
@@ -1,13 +1,29 @@
 package org.example.domain;
-
+import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
 
-@Entity
+@Table(name = "student_table")
+@Entity(name = "student")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
     public Long id;
+    @Column(unique = true)
     public String name;
     public String cls;
+
+    @Temporal(TemporalType.DATE)
+    public Date birthdate;
+
+    public Student(String name, String cls,Date birthdate) {
+        this.name = name;
+        this.cls = cls;
+        this.birthdate = birthdate;
+    }
+
 }
